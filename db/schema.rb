7122e7b8e1f159273c396d93bec5e8cf52f7fa19
Andrew Njoo <adnjoo@gmail.com>
2024-10-20 13:02:52 -0600

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_19_123852) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_20_190149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,20 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_123852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "plan_id"
+    t.string "customer_id"
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.datetime "current_period_end"
+    t.datetime "current_period_start"
+    t.string "interval"
+    t.string "subscription_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +52,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_123852) do
   end
 
   add_foreign_key "notes", "users"
+  add_foreign_key "subscriptions", "users"
 end
