96238029468414fe01bf6ac0ecc342b3df0eff58
Andrew Njoo <adnjoo@gmail.com>
2024-10-20 12:44:32 -0600

    -

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Stripe
-  validates :stripe_id, presence: true, uniqueness: true
-
-  before_validation :create_stripe_reference, on: :create
-
-  def create_stripe_reference
-    response = Stripe::Customer.create(email: email)
-    self.stripe_id = response.id
-  end
-
-  def retrieve_stripe_reference
-    Stripe::Customer.retrieve(stripe_id)
-  end
 end
