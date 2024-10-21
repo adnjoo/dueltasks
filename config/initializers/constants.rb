0adf199abc3c13b463bdda2bf9978e23ddead0e2
Andrew Njoo <adnjoo@gmail.com>
2024-10-20 21:54:13 -0600

    real money

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,5 +2,5 @@ APP_NAME = "DrewApp"
 GITHUB_LINK = "https://github.com/adnjoo/drewapp"
 
 # Subscription Prices
-SUBSCRIPTION_PRICE_MONTHLY = 5
+SUBSCRIPTION_PRICE_MONTHLY = 1
 SUBSCRIPTION_PRICE_ANNUAL = 50
