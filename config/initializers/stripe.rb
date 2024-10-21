351172aaa0b177fff51fccae4672bb0b77fcd9e1
Andrew Njoo <adnjoo@gmail.com>
2024-10-20 21:04:29 -0600

    debug

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,3 +4,6 @@
 
 # TODO: investigate why not working in railway
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+# Debugging output to check if the key is being set
+puts "Stripe API key: #{Stripe.api_key}"
