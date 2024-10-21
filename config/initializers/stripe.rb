f7ecdba6051ba4341a76158751dd95591301dcd6
Andrew Njoo <adnjoo@gmail.com>
2024-10-20 20:46:25 -0600

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,6 @@
 # config/initializers/stripe.rb
 
-# Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
 
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# TODO: investigate why not working in railway
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
