ab3fc813cbff6f6c6d70c9a9b4cbbade79ce75b0
Andrew Njoo <adnjoo@gmail.com>
2024-10-22 06:28:07 -0600

    sanitize

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -13,11 +13,24 @@ class BlogController < ApplicationController
     end
   end
 
-
   def show
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
-    @post = markdown.render(File.read("app/views/posts/#{params[:slug]}.md"))
+    slug = sanitize_slug(params[:slug])
+    file_path = Rails.root.join("app", "views", "posts", "#{slug}.md")
+
+    if File.exist?(file_path)
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+      @post = markdown.render(File.read(file_path))
+    else
+      redirect_to blog_path, alert: "Post not found."
+    end
   rescue Errno::ENOENT
     redirect_to blog_path, alert: "Post not found."
+  end
+
+  private
+
+  def sanitize_slug(slug)
+    # Only allow alphanumeric, hyphens, and underscores in slugs
+    slug.gsub(/[^0-9a-z\-_]/i, "")
   end
 end
