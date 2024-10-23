1204a97910535e2d8a81ce0f12dfc9db2cccc359
Andrew Njoo <adnjoo@gmail.com>
2024-10-22 20:56:29 -0600

    default: false

--- db/migrate/20241023025452_add_username_and_public_to_users.rb
+++ db/migrate/20241023025452_add_username_and_public_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameAndPublicToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :username, :string
-    add_column :users, :public, :boolean
+    add_column :users, :public, :boolean, default: false
   end
 end
