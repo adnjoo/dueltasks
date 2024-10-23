b53f27d13adbee6c7e23635513075d9a186b59f6
Andrew Njoo <adnjoo@gmail.com>
2024-10-23 15:48:11 -0600

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,58 @@
 Rails.application.routes.draw do
-  # Devise
-  devise_for :users
+  require "sidekiq/web"
 
+  # Devise Authentication
+  devise_for :users
   devise_scope :user do
     authenticated :user do
       root to: "notes#index", as: :authenticated_root
     end
-
     unauthenticated do
       root to: "pages#home"
     end
   end
 
-  # Notes
+  # Sidekiq Admin Interface
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
+  # Notes (Task Management)
   resources :notes, except: [ :show ] do
     member do
       patch :archive, :toggle_completion
     end
-  end
-
-  get "leaderboard", to: "notes#leaderboard"
-
-  # Stripe
-  scope controller: :static do
-    get :pricing
-  end
-
-  namespace :purchase do
-    resources :checkouts, only: [ :create ] do
-      collection do
-        get "success", to: "checkouts#success"
-      end
+    collection do
+      get :leaderboard
     end
   end
 
-  resources :webhooks, only: :create
-
-  resources :subscriptions
-
   # Static Pages
-  get "about", to: "pages#about", as: :about
+  get "about", to: "pages#about"
+  get "pricing", to: "static#pricing"
 
   # Blog
-  get "/blog", to: "blog#index"
-  get "/blog/:slug", to: "blog#show", as: "blog_post"
+  get "blog", to: "blog#index"
+  get "blog/:slug", to: "blog#show", as: "blog_post"
 
-  # API namespace for isolated routes
+  # Stripe (Subscriptions and Webhooks)
+  namespace :purchase do
+    resources :checkouts, only: [ :create ] do
+      get "success", on: :collection
+    end
+  end
+  resources :subscriptions, only: [ :index, :create, :destroy ]
+  post "webhooks", to: "webhooks#create"
+
+  # API Namespace
   namespace :api do
-    get "hello", to: proc { [ 200, { "Content-Type" => "application/json" }, [ '{"message":"Hello, World!"}' ] ] }
+    get "hello", to: proc { [ 200, { "Content-Type" => "application/json" }, [ '{"message":"Hello, World!"' ] ] }
   end
 
-  # Health check route for load balancers and uptime monitors
-  get "up", to: "rails/health#show", as: :health_check
+  # Health Check
+  get "up", to: "rails/health#show"
 
-  # PWA related routes
-  get "service-worker", to: "rails/pwa#service_worker", as: :service_worker
-  get "manifest", to: "rails/pwa#manifest", as: :pwa_manifest
+  # PWA Routes
+  get "service-worker", to: "rails/pwa#service_worker"
+  get "manifest", to: "rails/pwa#manifest"
 end
