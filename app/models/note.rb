a3f64a86c1e53a88c3203cbcf92364c259ec67f6
Andrew Njoo <adnjoo@gmail.com>
2024-10-23 19:40:41 -0600

    rails generate sidekiq:job Penalty

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,14 @@ class Note < ApplicationRecord
 
   belongs_to :user
 
+  # After saving the note, schedule the penalty check if the deadline is changed
+  after_save :schedule_penalty_check, if: :deadline_changed?
+
+
   validates :title, presence: true
-  # content validation removed, allowing empty content
+
+  def schedule_penalty_check
+    # Schedule PenaltyWorker to run 1 minute after the deadline
+    PenaltyWorker.perform_at(self.deadline + 1.minute, self.id)
+  end
 end
