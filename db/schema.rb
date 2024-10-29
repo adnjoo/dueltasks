b077e3f851064d1bbf2e00002a10d46507932127
Andrew Njoo <adnjoo@gmail.com>
2024-10-29 12:19:36 -0500

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_015026) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_171907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_015026) do
     t.string "username"
     t.boolean "public", default: false
     t.boolean "admin"
+    t.string "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true, where: "(username IS NOT NULL)"
