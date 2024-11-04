593ba8d109778073d026ebc7b4a47fa4d5157ae2
Andrew Njoo <adnjoo@gmail.com>
2024-11-04 02:59:09 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   require "sidekiq/web"
 
   # Devise Authentication
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations", omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     authenticated :user do
       root to: "notes#index", as: :authenticated_root
